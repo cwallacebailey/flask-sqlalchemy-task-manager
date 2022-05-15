cb0dbeaea0a934b26ef019380ac063dc42f7398b
cwallacebailey <cwallacebailey@gmail.com>
2022-05-15 17:35:47 +0000

    added basic functionality with navbar and footer

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+// links to materialize sidenav bar
+
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, options);
+  });
