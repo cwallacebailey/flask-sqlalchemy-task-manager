36522ebb123c6c40badafccdc4f11b6ab255cd5e
cwallacebailey <cwallacebailey@gmail.com>
2022-06-04 11:16:25 +0000

    Again, more update to databse

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -10,8 +10,9 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-if os.environ.get("DEVELOPMENT") == "TRUE"
+if os.environ.get("DEVELOPMENT") == "TRUE":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 db = SQLAlchemy(app)
